12aaa330d57912d8a389a0bac03d525460cd1d10
Abraão Filipi dos Santos <107932922+abraaosantosdeveloper@users.noreply.github.com>
2024-10-21 00:12:56 -0300

    Analytics Update

--- app/layout.js
+++ app/layout.js
@@ -24,7 +24,8 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+        >
+        <Analytics/>
         <Nav />
         {children}
       </body>
